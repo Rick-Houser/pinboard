8dfc2cba32229bdd25e25dd9292f918f558cf06f
FrackenToaster <frakentoaster@gmail.com>
2015-08-05 17:26:59 -0700

    Pins CRUD ability

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
 	before_action :find_pin, only: [:show, :edit, :update, :destroy]
-	
+
 	def index
+		@pins = Pin.all.order("created_at DESC")
 	end
 
 	def new
@@ -16,6 +17,22 @@ class PinsController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @pin.update(pin_params)
+			redirect_to @pin, notice: "Pin was successfully updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to root_path
 	end
 
 	private
